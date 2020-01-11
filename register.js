76b35d69b0c2842e0c0397a5d38d453589d67c47
Wei Zhang <sheepchina@hotmail.com>
2020-01-10 23:50:20 -0600

    Update the register.js

--- register.js
+++ register.js
@@ -1,8 +1,8 @@
 if ('serviceWorker' in navigator) {
     console.log('cool');
-      self.addEventListener('load', () => {
-        window.navigator.serviceWorker
-        .register('sw.js')
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+        .register("sw.js")
         .then((e) => console.log('the load event was fired by the browser %0' + e))
         .catch((error) => console.log('whoops, %0' + error));
       })
